5f8037ae3f347214a4f0ec6af024e88cfc1a510a
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-04-22 05:01:54 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -10,7 +10,27 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit-5-01-JS/sw.js", {
-    scope: "/ICS2O-Unit-5-01-JS/",
-  })
+  navigator.serviceWorker.register(
+    "/ICS2O-Unit-5-01-JS/sw.js",
+    {
+      scope: "/ICS2O-Unit-5-01-JS/",
+    }
+  )
+}
+
+/**
+ * This function updates the slider value.
+ */
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
+  document.getElementById("demo").innerHTML =
+Math.floor(Math.random() * 6) + 1;
+}
+
+/**
+ * This function displays the slider value.
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML =
+    "<p>Value is: " + slider.value + "</p>"
 }
