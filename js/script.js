82476b99508cbc703952ec51138537c218d75c6b
KaitlynIp64 <KaitlynIp64@users.noreply.github.com>
2022-04-25 16:50:01 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -32,12 +32,14 @@ function myButtonClicked() {
 
   // process
   if (sliderValue == randomNumber) {
-    document.getElementById('answer').innerHTML = 'Answer is ' + randomNumber +'!' +  ' Congratulations!'
-   // console.log("Congratulations!")
+    document.getElementById("answer").innerHTML =
+      "Answer is " + randomNumber + "!" + " Congratulations!"
+    // console.log("Congratulations!")
   }
   //  block of code to be executed if condition1 is true
   if (sliderValue != randomNumber) {
-    document.getElementById('answer').innerHTML = 'Answer is ' + randomNumber +'!' +  ' Try again!'
-   // console.log("Try again!")
+    document.getElementById("answer").innerHTML =
+      "Answer is " + randomNumber + "!" + " Try again!"
+    // console.log("Try again!")
   }
 }
